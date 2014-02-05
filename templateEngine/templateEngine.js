869bcc432a7eacd751e09b72b8c72c302ea33b3a
deleter <deleter8@gmail.com>
2014-02-04 19:14:17 -0500

    clean and split out examples
    Clean up the implementation / naming. Encapsulate into more of a
    module. Split out example tests

--- templateEngine/templateEngine.js
+++ templateEngine/templateEngine.js
@@ -1,36 +1,23 @@
 var peg = require("pegjs");
 
-var parser = peg.buildParser(
-    "start = blob " +
-        "blob = body:(variable_ref)+ " +
-        "variable_ref = '{{' variable:variable '}}' {return {'token':'var','data':variable};} / text " +
-        "variable = variable:(symbol (variable_property)*) {if(variable[1].length>0){return variable[0] + '.' + variable[1].join('.');}else{return variable[0];} } " +
-        "variable_property = '.' symbol:symbol {return symbol;} " +
-        "text = text_value:([^\\{]+) {return {'token':'text', 'data':text_value.join('')};} / foreach "+
-        "foreach = '{%' ws 'foreach' ws iter:symbol ws 'in' ws list:symbol ws '%}' newline:([ \t]* ('\\n'))? body:blob  '{%' ws 'end' ws 'foreach' ws '%}' endnewline:('\\n')? {return {'token':'foreach', 'var':iter, 'list':list, 'newline':!!newline, 'body':body}; } " +
-        "symbol = symbol:([A-Za-z][A-Za-z0-9]*) {if(symbol.length>1){return symbol[0] + symbol[1].join('');}else{ return symbol.join('')};} " +
-        "ws = ([ \\t\\n\\r])+ "+
-        ""
-);
-
-function Stack(){
-    this.scopes = [{}];
+function Environment(init){
+    if(!init) init = {};
+    this.scopes = [init];
 }
 
-Stack.prototype.push = function(){
+Environment.prototype.push = function(){
     this.scopes.push({});
 };
 
-Stack.prototype.pop = function(){
+Environment.prototype.pop = function(){
     this.scopes.pop();
 };
 
-Stack.prototype.set = function(name, val){
+Environment.prototype.set = function(name, val){
     this.scopes[this.scopes.length-1][name] = val;
 };
 
-Stack.prototype.get = function(name){
-    console.log('--looking for variable:' + name);
+Environment.prototype.get = function(name){
     for(var i = this.scopes.length - 1; i >= 0; i--){
         if(this.scopes[i].hasOwnProperty(name)){
             return this.scopes[i][name];
@@ -39,11 +26,11 @@ Stack.prototype.get = function(name){
     return undefined;
 };
 
-function evalTree(tree, stack){
+function evaluateTree(tree, environment){
     if(tree instanceof Array){
         return tree
             .map(function eval(subtree){
-                return evalTree(subtree, stack);
+                return evaluateTree(subtree, environment);
             })
             .reduce(function combine(accum, subtree){
                 return accum + subtree;
@@ -57,7 +44,7 @@ function evalTree(tree, stack){
 
                 if(~varName.indexOf('.')){
                     var elements = varName.split('.');
-                    var deref = stack.get(elements[0]);
+                    var deref = environment.get(elements[0]);
                     for(var i = 1; i < elements.length; i++){
                         if(deref.hasOwnProperty(elements[i])){
                             deref = deref[elements[i]];
@@ -67,50 +54,49 @@ function evalTree(tree, stack){
                     }
                     return deref;
                 }
-                return stack.get(tree['data']);
+                return environment.get(tree['data']);
             }
             case 'foreach':
             {
-                var list = stack.get(tree['list']);
+                var list = environment.get(tree['list']);
                 var value = '';
-                // dont think i need this - if(tree['newline']) value = '\n';
-                stack.push();
+                environment.push();
                 for (var i = 0; i < list.length; i++){
-                    stack.set(tree['var'], list[i]);
-                    value = value + evalTree(tree['body'], stack);
+                    environment.set(tree['var'], list[i]);
+                    value = value + evaluateTree(tree['body'], environment);
                 }
-                stack.pop();
+                environment.pop();
                 return value;
             }
         }
-    }else{
-        throw "error evaluating tree node: " + JSON.stringify(tree);
     }
+
+    throw "error evaluating tree node: " + JSON.stringify(tree);
 }
 
-//-----------------------------------------------------------------------------
-//Some test code:
+function Engine(){
+    this.parser = peg.buildParser(
+        "start = blob " +
+            "blob = body:(variable_ref)+ " +
+            "variable_ref = '{{' variable:variable '}}' {return {'token':'var','data':variable};} / text " +
+            "variable = variable:(symbol (variable_property)*) {if(variable[1].length>0){return variable[0] + '.' + variable[1].join('.');}else{return variable[0];} } " +
+            "variable_property = '.' symbol:symbol {return symbol;} " +
+            "text = text_value:([^\\{]+) {return {'token':'text', 'data':text_value.join('')};} / foreach "+
+            "foreach = '{%' ws 'foreach' ws iter:symbol ws 'in' ws list:symbol ws '%}' newline:([ \t]* ('\\n'))? body:blob  '{%' ws 'end' ws 'foreach' ws '%}' endnewline:('\\n')? {return {'token':'foreach', 'var':iter, 'list':list, 'newline':!!newline, 'body':body}; } " +
+            "symbol = symbol:([A-Za-z][A-Za-z0-9]*) {if(symbol.length>1){return symbol[0] + symbol[1].join('');}else{ return symbol.join('')};} " +
+            "ws = ([ \\t\\n\\r])+ "+
+            ""
+    );
 
-var tree = parser.parse(
-    "abuncho \t\nftext{{var1}} alskdjf {{var2}}\nnowforeach\n" +
-        "{% foreach sublist in list %}\n"+
-        "{% foreach item in sublist %}\n"+
-        "    text({{item}}),\n" +
-        "{% end foreach %}\n" +
-        "{% end foreach %}\n" +
-        "\n" +
-        "o:{{obj.b.d}}\n" +
-        "end"
+}
 
-);
+Engine.prototype.processTemplate = function(template, environment){
+    var tree = this.parser.parse(template);
+    return evaluateTree(tree, environment);
+};
 
-console.log(JSON.stringify(tree));
+module.exports = {
+    'Engine':Engine,
+    'Environment':Environment
+};
 
-var stack = new Stack();
-stack.set("var1", "foo");
-stack.set("var2", "2");
-stack.set("obj", {'a':'OBJ_A_VALUE', 'b': {'c':'OBJ_B_C_VALUE', 'd':'OBJ_B_D_VALUE'}});
-stack.set("list", [['a','b','c'],['a2','b2','c2'],['a3','b3','c3']]);
-
-console.log(evalTree(tree, stack));
-//-----------------------------------------------------------------------------
---
+++ templateEngine/test.js
@@ -0,0 +1,50 @@
+var Engine = require('./templateEngine.js').Engine;
+var Environment = require('./templateEngine.js').Environment;
+
+//-----------------------------------------------------------------------------
+//Some test code:
+
+var engine = new Engine();
+
+configData =
+    "mysettingheader:\n" +
+        "    some_static_setting=boring\n" +
+        "    some_exciting_setting={{variable1}}\n" +
+        "\n" +
+        "headerwithvariableentries:\n" +
+        "#list1 has to be a javascript array in the given environment\n" +
+        "{% foreach item in list1 %}\n" +
+        "    setting {{item.name}} {{item.host}}:{{item.port}} #whitespace is preserved\n" +
+        "{% end foreach %}\n";
+
+env = new Environment({
+    "variable1": "foo",
+    "list1": [
+        {'name':'foo', 'host':'127.0.0.1', 'port':'80'},
+        {'name':'bar', 'host':'127.0.0.1', 'port':'443'}
+    ]
+});
+
+console.log(engine.processTemplate(configData, env));
+
+//-----------------------------------------------------------------------------
+//test with nested foreach
+
+var configData =
+    "a bunch of \ntext\t{{var1}}\n" +
+        "{% foreach sublist in list %}\n"+
+        "{% foreach item in sublist %}\n"+
+        "    text({{item}},{{var2}})\n" +
+        "{% end foreach %}\n" +
+        "{% end foreach %}\n" +
+        "\n" +
+        "o:{{obj.b.d}}\n" +
+        "end";
+
+var env = new Environment();
+env.set("var1", "foo");
+env.set("var2", "2");
+env.set("obj", {'a':'OBJ_A_VALUE', 'b': {'c':'OBJ_B_C_VALUE', 'd':'OBJ_B_D_VALUE'}});
+env.set("list", [['a','b','c'],['a2','b2','c2'],['a3','b3','c3']]);
+
+console.log(engine.processTemplate(configData, env));
