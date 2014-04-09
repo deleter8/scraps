c16935a3902992358486ccdd47a1b0e2cbb83d93
deleter <deleter8@gmail.com>
2014-04-09 17:05:18 -0400

    refactor a bit for clarity

--- utils/replace-recursive.js
+++ utils/replace-recursive.js
@@ -1,37 +1,41 @@
 var Q = require('q');
 
+function validArrayIndex(index) {
+    return /^0$|^[1-9]\d*$/.test(index) && index < 4294967295;
+}
+
 function replaceRecursive(value, test, replace) {
     var promises = [];
 
     //If tests positive, replace
     return Q.fcall(function(){ return test(value); })
         .then(function(testedPositive){
-        if (testedPositive){
-            return replace(value);
-        }else{
-            //Walk through array or object, replacing any positive matches
-            if (value instanceof Array || value instanceof Object) {
-                for (var index in value) {
-                    if (value.hasOwnProperty(index)
-                        && (!(value instanceof Array) || (/^0$|^[1-9]\d*$/.test(index) && index <= 4294967294))) {
-                        (function iterate(index) {
-                            promises.push(Q.fcall(function(){
-                                return replaceRecursive(value[index], test, replace).then(function (data) {
-                                    value[index] = data;
-                                })})
-                            );
-                        })(index);
+            if (testedPositive){
+                return replace(value);
+            }else{
+                //Walk through array or object, replacing any positive matches
+                if (value instanceof Array || value instanceof Object) {
+                    for (var index in value) {
+                        if (value.hasOwnProperty(index)
+                            && (!(value instanceof Array) || validArrayIndex(index))) {
+                            (function iterate(index) {
+                                promises.push(Q.fcall(function(){
+                                        return replaceRecursive(value[index], test, replace).then(function (data) {
+                                            value[index] = data;
+                                        })})
+                                );
+                            })(index);
+                        }
                     }
                 }
-            }
 
-            return Q.allSettled(promises).then(function () {
-                return Q.fcall(function () {
-                    return value;
+                return Q.allSettled(promises).then(function () {
+                    return Q.fcall(function () {
+                        return value;
+                    });
                 });
-            });
-        }
-    });
+            }
+        });
 }
 
 function replaceRecursiveSync(value, test, replace) {
@@ -44,7 +48,7 @@ function replaceRecursiveSync(value, test, replace) {
     if (value instanceof Array || value instanceof Object) {
         for (var index in value) {
             if (value.hasOwnProperty(index)
-                && (!(value instanceof Array) || (/^0$|^[1-9]\d*$/.test(index) && index <= 4294967294))) {
+                && (!(value instanceof Array) || validArrayIndex(index))) {
                 value[index] = replaceRecursiveSync(value[index], test, replace);
             }
         }
